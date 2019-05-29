5dbda5d4958d0a6b58ba4ff5b2843b3d7978a799
motasem6236 <motasem6236@gmail.com>
2019-05-29 21:49:13 +0300

    motasem

--- angular-client/components/login.js
+++ angular-client/components/login.js
@@ -1,33 +1,103 @@
-(function () {
-    'use strict';
-    angular
-        .module('app')
-        .controller('LoginController', LoginController);
+var myApp = angular.module("myApp", []);
+myApp.service("ContactService" , function(){
+ var uid = 1;
+ var contacts = [{
+   'id' : 0,
+ 'name' : 'Steve John',
+ 'email' : 'john@gmail.com',
+ 'password': 'John123',
+ 'phone' : '911-91-199-999'}]; 
  
-    LoginController.$inject = ['$location','AuthenticationService'];
-    function LoginController( $location, AuthenticationService) {
-        var vm = this;
-        
-        vm.login = login;
+ // Save Service for sving new contact and saving existing edited contact.
+ this.save = function(contact)  
+ {
+ if(contact.id == null)                       
+ {
+ contact.id = uid++;
+ contacts.push(contact);
+ }
+ else
+ {
+ for(var i in contacts)
+ {
+ if(contacts[i].id == contact.id)
+ {
+ contacts[i] = contact;
+ }
+ }
+ }
+ };
  
-        (function initController() {
-            // reset login status
-            AuthenticationService.ClearCredentials();
-        })();
-        
-        function login() {
-          console.log("login method executing");
-          
-          vm.dataLoading = true;
-            AuthenticationService.Login(vm.username, vm.password, function (response) {
-                if (response.success) {
-                    AuthenticationService.SetCredentials(vm.username, vm.password);
-                    $location.path('/home');
-                } else {
-                    alert(response.message);
-                    vm.dataLoading = false;
-                }
-        });
-      }
-    }
-})();
+ // serach for a contact
+ 
+ this.get = function(id)
+ {
+ for(var i in contacts )
+ {
+ if( contacts[i].id == id)
+ {
+ return contacts[i];
+ }
+ }
+ };
+ 
+ //Delete a contact
+ this.delete = function(id)
+ {
+ for(var i in contacts)
+ {
+ if(contacts[i].id == id)
+ {
+ contacts.splice(i,1);
+ }
+ }
+ }; 
+ //Show all contacts
+ this.list = function()
+ {
+ return contacts;
+ } ; 
+});
+ 
+////Controller area .....
+ 
+myApp.controller("ContactController" , function($scope , ContactService){
+    console.clear();
+    
+    $scope.ifSearchUser = false;
+    $scope.title ="List of Users";
+    
+ $scope.contacts = ContactService.list();
+ 
+ $scope.saveContact = function()
+ {
+   console.log($scope.newcontact);
+   if($scope.newcontact == null || $scope.newcontact == angular.undefined)
+   return;
+ ContactService.save($scope.newcontact);
+ $scope.newcontact = {};
+ }; 
+ $scope.delete = function(id)
+ {
+ ContactService.delete(id);
+ if($scope.newcontact != angular.undefined && $scope.newcontact.id == id)
+ {
+ $scope.newcontact = {};
+ }
+ }; 
+ $scope.edit = function(id)
+ {
+ $scope.newcontact = angular.copy(ContactService.get(id));
+ }; 
+ $scope.searchUser = function(){
+   if($scope.title == "List of Users"){
+     $scope.ifSearchUser=true;
+     $scope.title = "Back";
+   }
+   else
+   {
+     $scope.ifSearchUser = false;
+     $scope.title = "List of Users";
+   }   
+ };
+});
