c9c500362cc2ddd02ec5ccacb70bf3730451244e
RawanAkAzz <rawan.akram27@yahoo.com>
2019-05-29 13:11:25 +0300

    rawan

---
+++ angular-client/components/Rigistration.js
@@ -0,0 +1,33 @@
+// var myApp = angular.module("myApp", []);
+// myApp.controller("RegisterCtrl", function ($scope) {
+
+// });
+(function () {
+    'use strict';
+ 
+    angular
+        .module('app')
+        .controller('RegisterController', RegisterController);
+ 
+    RegisterController.$inject = ['UserService', '$location'];
+    function RegisterController(UserService, $location) {
+        var vm = this;
+ 
+        vm.dataLoading = false;
+        vm.register = register;
+ 
+        function register() {
+            vm.dataLoading = true;
+            UserService.create(vm.user)
+                .then(function (response) {
+                    if (response.success) {
+                        alert('Registration successful');
+                        $location.path('/');
+                    } else {
+                        alert(response.message);
+                        vm.dataLoading = false;
+                    }
+            });
+        }
+    }
+})();
